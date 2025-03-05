24003beafadbe8e976e80b2aa3d201b52724449d
dkempfen <d.kempfen@gmail.com>
2025-03-05 13:49:40 -0300

    Descripción de los cambios que realizaste

--- firebase.js
+++ firebase.js
@@ -1,11 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs, query, where, orderBy, startAfter, limit } from "firebase/firestore";
-import { getAuth } from "firebase/auth"
-
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -21,4 +17,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-export { db, collection, getDocs, query, where, orderBy, startAfter, limit };
+
+// Exporta solo los métodos necesarios
+export { collection, getDocs, query, where, orderBy, startAfter, limit };
